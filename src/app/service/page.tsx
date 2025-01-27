c63b548dfc44e9d3468a987cbbd54e94eae29d0e
KAKAKANG2450 <s46.poorinut@gmail.com>
2025-01-27 10:02:15 +0700

    in class changedd

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -10,7 +10,7 @@ const service = () => {
       <Navbar />
 
       {/* Hero Section */}
-      <div className="bg-cover bg-center h-screen text-center text-white bg-[url(/img/ServicePicture.jpg)]">
+      <div className="bg-cover bg-center h-screen text-center text-white bg-[url(/img/servicesTop.jpg)]">
         <h1 className="text-2xl md:text-7xl font-bold italic pt-55 pl-18 absolute hidden md:block">
           Services
         </h1>
@@ -37,23 +37,23 @@ const service = () => {
       {/* Services Section */}
       <div className="grid md:grid-cols-2 grid-cols-1 grid-rows-2 gap-4 bg-custom1 p-10">
         <div className="bg-cover bg-center h-screen w-full bg-[url(/img/floorPlan.jpg)] ">
-          <div className="text-start text-custom1 text-8xl font-bold pl-10 pt-10">
+          <div className="text-start text-custom1 text-6xl font-bold pl-10 pt-10">
             AI Generate <br />
             Floor Plan
           </div>
         </div>
         <div className="bg-cover bg-center h-screen w-full bg-[url(/img/VR.png)] ">
-          <div className="text-start text-white text-9xl font-bold pl-10 pt-10">
+          <div className="text-start text-white text-8xl font-bold pl-10 pt-10">
             VR
           </div>
         </div>
         <div className="bg-cover bg-center h-screen w-full bg-[url(/img/consult.png)] ">
-          <div className="text-start text-custom1 text-8xl font-bold pl-10 pt-10">
+          <div className="text-start text-custom1 text-7xl font-bold pl-10 pt-10">
             Consult
           </div>
         </div>
         <div className="bg-cover bg-center h-screen w-full bg-[url(/img/augmentedReality.png)] ">
-          <div className="text-start text-white text-9xl font-bold pl-10 pt-10">
+          <div className="text-start text-white text-8xl font-bold pl-10 pt-10">
             AR
           </div>
         </div>
