ab35dba892fa108d33bc1e55deadf42fc19065d2
Nine <rbxninez@gmail.com>
2025-02-02 19:45:40 +0700

    fix navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,35 +7,38 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="flex md:justify-around justify-between items-center p-4 bg-custom1 text-white">
-      <div className="text-2xl font-bold">Tech Home</div>
-      <ul className="md:flex space-x-8 text-NavbarFontColor hidden">
-        <li>
-          <Link href="/" className="font-semibold">
-            Home
+    <nav className="flex mx-auto md:justify-center justify-between items-center p-4 bg-custom1 text-white">
+      <div className="flex justify-between w-full items-center">
+        <div className="text-2xl font-bold">Tech Home</div>
+        <ul className="md:flex space-x-8 text-NavbarFontColor hidden">
+          <li>
+            <Link href="/" className="font-semibold">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="font-semibold">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/service" className="font-semibold">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/project" className="font-semibold">
+              Projects
+            </Link>
+          </li>
+        </ul>
+        <button className="bg-custom2 text-custom1 px-4 py-2 rounded-lg hidden md:block ">
+          <Link href="/contact" className="font-semibold">
+            Contact Us
           </Link>
-        </li>
-        <li>
-          <Link href="/about" className="font-semibold">
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/service" className="font-semibold">
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link href="/project" className="font-semibold">
-            Projects
-          </Link>
-        </li>
-      </ul>
-      <button className="bg-custom2 text-custom1 px-4 py-2 rounded-lg hidden md:block ">
-        <Link href="/contact" className="font-semibold">
-          Contact Us
-        </Link>
-      </button>
+        </button>
+      </div>
+
       <button
         onClick={() => {
           setOpen(!open);
