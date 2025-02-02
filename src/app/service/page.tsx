02f2de2f050048c845297d8bf86d56c919ae8a66
KAKAKANG2450 <s46.poorinut@gmail.com>
2025-02-02 19:31:32 +0700

    new

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import Image from "next/image";
 
 const service = () => {
   return (
